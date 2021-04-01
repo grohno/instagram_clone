92bf855a6dc3e82b41d4564f052951d9c0f44873
yuki ohno <jeanadler33@gmail.com>
2021-04-01 04:26:59 +0000

    user/picture/favoriteの rails g と db:migrate を実行

---
+++ db/migrate/20210401041644_create_pictures.rb
@@ -0,0 +1,12 @@
+class CreatePictures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pictures do |t|
+      t.string :title
+      t.text :content
+      t.text :image
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
