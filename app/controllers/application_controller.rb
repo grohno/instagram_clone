c09f6652a42f4c0aff7993f79c5cd93a4f7f9351
yuki ohno <jeanadler33@gmail.com>
2021-04-02 08:03:23 +0000

    Bootstrapのインストールと設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :login_required
+
   private
   def login_required
     redirect_to new_session_path unless current_user
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,12 @@ class FavoritesController < ApplicationController
   def index
     @favorites = current_user.favorites.all
   end
+
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
   end
+  
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
