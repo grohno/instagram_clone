dc24653dc753326b48dfb03293915d7807f7989b
yuki ohno <jeanadler33@gmail.com>
2021-04-02 04:30:07 +0000

    お気に入り一覧表示画面の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,7 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = current_user.favorites.all
+  end
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
 end
