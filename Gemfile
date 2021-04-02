b17274cea35f945e2c26454c3b435160aa249cd2
yuki ohno <jeanadler33@gmail.com>
2021-04-02 07:48:10 +0000

    Userに関するseedデータの実装

--- Gemfile
+++ Gemfile
@@ -37,3 +37,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'bcrypt', '3.1.11'
+gem 'faker'
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,7 @@
-I18n.config.available_locales = :ja
-I18n.default_locale = :ja
+require 'i18n'
+I18n.locale # => :en
+I18n.locale = :ja
+I18n.locale # => :ja
+require 'faker'
+Faker::Config.locale # => :ja
+Faker::Internet.email # => ".@.com"
