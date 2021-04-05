731dd4a2a344716b39e67aea73b4d38119cbf95e
yuki ohno <jeanadler33@gmail.com>
2021-04-05 05:28:50 +0000

    デザイン実装

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,7 +1,2 @@
-require 'i18n'
-I18n.locale # => :en
-I18n.locale = :ja
-I18n.locale # => :ja
-require 'faker'
-Faker::Config.locale # => :ja
-Faker::Internet.email # => ".@.com"
+I18n.config.available_locales = :ja
+I18n.default_locale = :ja
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  root 'pictures#index'
   resources :pictures do
     collection do
       post :confirm
